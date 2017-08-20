eeaec2d5c3b37eca14f3e61185f1addc95760920
menno <mennoluimstra@hotmail.com>
2017-08-20 19:37:34 +0200

    initial commit

--- ClientUI/Program.cs
+++ ClientUI/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NServiceBus;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,27 @@ namespace ClientUI
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static void Main()
 		{
+			AsyncMain().GetAwaiter().GetResult();
+		}
+
+		static async Task AsyncMain()
+		{
+			Console.Title = "ClientUI";
+
+			var endPointConfiguration = new EndpointConfiguration("ClientUI");
+
+			var transport = endPointConfiguration.UseTransport<LearningTransport>();
+
+			var endpointInstance = await Endpoint.Start(endPointConfiguration)
+				.ConfigureAwait(false);
+
+			Console.WriteLine("Press Enter to exit...");
+			Console.ReadLine();
+
+			await endpointInstance.Stop()
+				.ConfigureAwait(false);
 		}
 	}
 }
